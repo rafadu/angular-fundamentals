44f7cce6f75f7d5fe86def537bbe661748339568
Rafael Duarte <rafael.dgs.1993@gmail.com>
2025-11-01 11:48:16 -0300

    cap 8

--- joes-robot-shop/src/app/cart.service.ts
+++ joes-robot-shop/src/app/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './catalog/product.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,12 @@ import { IProduct } from './catalog/product.model';
 export class CartService {
   cart :IProduct[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   add(product: IProduct){
     this.cart.push(product);
-    console.log(`product ${product.name} added to cart`)
+    this.http.post('/api/cart', this.cart).subscribe(() => {
+      console.log(`product ${product.name} added to cart`)
+    });
   }
 }

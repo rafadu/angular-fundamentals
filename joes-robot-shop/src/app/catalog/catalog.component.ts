1bad788571c32e8056b891e301b6028b6773c1f1
Rafael Duarte <rafael.dgs.1993@gmail.com>
2025-11-02 12:56:28 -0300

    cap 9

--- joes-robot-shop/src/app/catalog/catalog.component.ts
+++ joes-robot-shop/src/app/catalog/catalog.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { IProduct } from './product.model';
-import { CartService } from '../cart.service';
+import { CartService } from '../cart/cart.service';
 import { ProductService } from './product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'bot-catalog',
@@ -15,7 +16,11 @@ export class CatalogComponent {
   //private cartSvc: CartService = inject(CartService);
   
 
-  constructor(private cartSvc: CartService, private productSvc: ProductService) {
+  constructor(private cartSvc: CartService, 
+              private productSvc: ProductService,
+              private router: Router,
+              private route: ActivatedRoute) 
+  {
     this.products = [];
   }
 
@@ -23,10 +28,18 @@ export class CatalogComponent {
     this.productSvc.getProducts().subscribe(products => {
       this.products = products;
     });
+    //this.filter = this.route.snapshot.params['filter'];
+    // this.route.params.subscribe((params)=>{
+    //   this.filter = params['filter'] ?? '';
+    // });
+    this.route.queryParams.subscribe((params)=>{
+      this.filter = params['filter'] ?? '';
+    });
   }
 
   addToCart(product: IProduct){
    this.cartSvc.add(product);
+   this.router.navigate(['/cart']);
   }
  
 
